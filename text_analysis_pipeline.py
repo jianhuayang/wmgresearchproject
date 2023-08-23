73162e3c5c442af7b2de653ad745e82a7f10ee47
gongersio <samakshagarwal27@gmail.com>
2023-08-23 08:58:24 +0100

    Specify text file name

--- text_analysis_pipeline.py
+++ text_analysis_pipeline.py
@@ -10,6 +10,11 @@ import pytextrank
 start_video = 1
 end_video = 10
 
+def clear_file(file_name):
+    '''Clear the text file.'''
+    with open(file_name,'w') as file:
+        pass
+
 def audio_extraction(file_path):
     '''Loads the video from a given file path and extracts the audio into a wav file, ensuring it is in the correct format.'''
     video = AudioSegment.from_file(file_path, format = "mp4")
@@ -28,20 +33,20 @@ def speech_recognition():
     with open(file_name, "w") as file:
         file.write(text)
 
-def keyphrase_extraction():
+def keyphrase_extraction(file_name):
     '''Performs keyphrase extraction on the transcript using the spaCy model, appending all unique keyphrases to a text file.'''
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("textrank")
     doc = nlp(Path("transcript.txt").read_text(encoding="utf-8"))
 
-    file_name = "keyphrases.txt"
     with open(file_name,"a") as file:
         keyphrases_list = [phrase.text for phrase in doc._.phrases if phrase.rank >=0] #Excludes stop words (keyphrases that convey no meaning).
         keyphrases = '\n'.join(keyphrases_list) + '\n'
         file.write(keyphrases)
 
-def main():
+def main(file_name="keyphrases.txt"):
     '''The output of the pipeline is a text file containing all the keyphrases from the entire subset of specified videos.'''
+    clear_file(file_name)
     for i in range(start_video,end_video+1):
         #The file path should follow the naming convention of the saved videos.
         file = "video" + str(i)
@@ -49,7 +54,7 @@ def main():
 
         audio_extraction(file_path)
         speech_recognition()
-        keyphrase_extraction()
+        keyphrase_extraction(file_name)
 
 if __name__ == "__main__":
     main()
