3aac77acf92a390cab186d4367860e4cce72ec82
gongersio <samakshagarwal27@gmail.com>
2023-08-23 06:13:48 +0100

    Update comments

--- text_analysis_pipeline.py
+++ text_analysis_pipeline.py
@@ -1,4 +1,4 @@
-#Import all the required libraries to perform the text analysis.
+#Import all the required libraries.
 import speech_recognition as sr
 from pydub import AudioSegment
 import os
@@ -24,14 +24,14 @@ for i in range(start_video,end_video+1):
     audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2) 
     audio.export("audio.wav", format="wav")
 
-    #Load the audio file and extract any speech from it.
+    #Load the audio file and extract the speech from it.
     r = sr.Recognizer()
     with sr.AudioFile("audio.wav") as source:
         audio_text = r.record(source)
 
     text = r.recognize_google(audio_text,language="en-US")
 
-    #Save a transcript of the speech as a text file.
+    #Write the transcript of the speech to a text file.
     file_name = "transcript.txt"
 
     with open(file_name, "w") as file:
@@ -40,7 +40,7 @@ for i in range(start_video,end_video+1):
     #Load the spaCy model in order to perform keyword extraction.
     nlp = spacy.load("en_core_web_sm")
     nlp.add_pipe("textrank")
-
     doc = nlp(Path("transcript.txt").read_text(encoding="utf-8"))
+    
     for phrase in doc._.phrases:
         print(phrase.text, phrase.rank, phrase.count) #A higher rank value means the phrase is more relevant.
