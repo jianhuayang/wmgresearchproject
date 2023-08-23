dad70d785fcf8ac71fc44f394c84c701591f9701
gongersio <samakshagarwal27@gmail.com>
2023-08-23 09:13:13 +0100

    Change file names

--- frequency_analysis.py
+++ frequency_analysis.py
@@ -2,12 +2,10 @@
 import text_analysis_pipeline as tap
 import re
 
-def main():
+def main(import_file="keyphrases.txt", export_file="frequency_analysis.txt"):
     '''Identifies the keyphrases that occur most frequently in keyphrases.txt, excluding stop words. The results are written to a text file. '''
-    tap.main()
-
     frequency = {}
-    keyphrase_doc = open("keyphrases.txt", "r")
+    keyphrase_doc = open(import_file, "r")
     keyphrase = keyphrase_doc.read().lower()
 
     #Use a regular expression to only include keywords that are between 3 and 15 characters.
@@ -25,11 +23,11 @@ def main():
     most_frequent = dict(sorted(frequency.items(), key=lambda elem: elem[1], reverse=True))
     most_frequent_count = most_frequent.keys()
     
-    file_name = "frequency_analysis.txt"
-    with open(file_name,"w") as file:
+    with open(export_file,"w") as file:
         keywords_list = [(word,most_frequent[word]) for word in most_frequent_count]
         keywords = '\n'.join(' '.join(map(str, tpl)) for tpl in keywords_list)
         file.write(keywords)
 
 if __name__ == "__main__":
+    tap.main()
     main()
