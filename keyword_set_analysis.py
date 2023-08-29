49340a5df484c608924cf269287c366806b23870
gongersio <samakshagarwal27@gmail.com>
2023-08-30 00:16:06 +0100

    Update set analysis

--- keyword_set_analysis.py
+++ keyword_set_analysis.py
@@ -1,22 +1,25 @@
 import text_analysis_pipeline as tap
 
 def main(file1_name,file2_name):
-    file1 = open(file1_name)
-    file2 = open(file2_name)
+    keywords_set = set()
+    keywords2_set = set()
 
-    keyphrases_set = set()
-    keyphrases2_set = set()
+    with open(file1_name, "r") as file:
+        for line in file:
+            words = line.strip().split(" ")
+            keyword = " ".join(words[:-1])
+            keywords_set.add(keyword)
 
-    for word in file1.read().split():
-        keyphrases_set.add(word.lower())
+    with open(file2_name, "r") as file:
+        for line in file:
+            words = line.strip().split(" ")
+            keyword = " ".join(words[:-1])
+            keywords2_set.add(keyword)
 
-    for word in file2.read().split():
-        keyphrases2_set.add(word.lower())
-
-    print(keyphrases_set.intersection(keyphrases2_set))
-    print(keyphrases_set.difference(keyphrases2_set))
+    print(keywords_set.intersection(keywords2_set))
+    print(keywords_set.difference(keywords2_set))
 
 if __name__ == "__main__":
-    tap.main(1,10)
-    tap.main(11,20,"keyphrases2.txt")
-    main("keyphrases.txt", "keyphrases2.txt")
+    tap.main(1,3)
+    tap.main(4,7,"keywords2.txt")
+    main("keywords.txt", "keywords2.txt")
