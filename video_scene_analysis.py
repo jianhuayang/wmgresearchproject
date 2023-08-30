af8855d98078ad9a3614cfda9669c0e3970bec4b
gongersio <samakshagarwal27@gmail.com>
2023-08-30 04:58:27 +0100

    Update csv with scene data

--- video_scene_analysis.py
+++ video_scene_analysis.py
@@ -1,5 +1,9 @@
-from scenedetect import open_video, SceneManager, AdaptiveDetector, FrameTimecode
+from scenedetect import open_video, SceneManager, AdaptiveDetector
+import pandas as pd
+
 def main(start_video=1, end_video=10):
+    avg_scene_length_list = []
+    num_scenes_list = []
 
     for i in range(start_video,end_video+1):
         #The file path should follow the naming convention of the saved videos.
@@ -13,6 +17,7 @@ def main(start_video=1, end_video=10):
 
         scenes = sm.get_scene_list(start_in_scene=True) 
         num_scenes = len(scenes)
+        num_scenes_list.append(num_scenes)
         total_length = 0
 
         for scene in scenes: 
@@ -20,8 +25,12 @@ def main(start_video=1, end_video=10):
             total_length+=scene_length
 
         avg_scene_length = round(total_length/num_scenes, 2)
-
-        print(num_scenes,avg_scene_length)
+        avg_scene_length_list.append(avg_scene_length)
+    
+    df = pd.read_csv("video_data.csv")
+    df["Number of Scenes"] = num_scenes_list
+    df["Average Scene Length"] = avg_scene_length_list
+    df.to_csv("video_data.csv")
 
 if __name__ == "__main__":
-    main()
+    main(1,40)
