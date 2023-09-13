566f257da8bb666de463aa16951cb14643d3e30b
gongersio <samakshagarwal27@gmail.com>
2023-09-13 09:51:11 +0100

    Remove full transcript + update comments

--- text_analysis_pipeline.py
+++ text_analysis_pipeline.py
@@ -16,26 +16,23 @@ def audio_extraction(file_path):
     audio = video.set_channels(1).set_frame_rate(16000).set_sample_width(2) 
     audio.export("audio.wav", format="wav")
 
-def speech_recognition(full_transcript):
+def speech_recognition():
     '''Loads the audio file and extracts the speech from it, writing the transcript to a text file.'''
     r = sr.Recognizer()
     with sr.AudioFile("audio.wav") as source:
         audio_text = r.record(source)
 
     try:
-        text = r.recognize_google(audio_text,language="en-US") #Google Speech Recognition model.
+        text = r.recognize_google(audio_text,language="en-US") #Uses Google's Speech Recognition model.
 
         file_name = "transcript.txt"
         with open(file_name, "w") as file:
             file.write(text)
 
-        with open(full_transcript,"a") as file:
-            file.write(text.lower() + " ")
-
     except sr.UnknownValueError:
         print("Could not transcribe.")
         file_name = "transcript.txt"
-        with open(file_name, "w"): pass
+        clear_file(file_name) #Ensure the transcript is cleared even if there is no speech.
 
 def keyword_extraction(kw_dict):
     '''
@@ -55,20 +52,20 @@ def keyword_extraction(kw_dict):
     except:
         punctuated_string = words_string
 
-    kw_extractor = yake.KeywordExtractor(lan="en",n=2,dedupLim=0.6)
+    kw_extractor = yake.KeywordExtractor(lan="en",n=2,dedupLim=0.4)
     keywords = kw_extractor.extract_keywords(punctuated_string)
 
+    #Record the cumulative frequency of each keyword.
     for kw, _ in keywords:
         kw = kw.lower()
         kw_dict[kw] = kw_dict.get(kw, 0) + 1
 
-def main(start_video=1, end_video=5, file_name="keywords.txt", full_transcript="full_transcript.txt"):
+def main(start_video=1, end_video=40, file_name="keywords.txt"):
     '''
     The output of the pipeline is a text file containing all unique keywords 
     and their cumulative frequency count across the entire subset of specified videos.
     '''
     clear_file(file_name)
-    clear_file(full_transcript)
     cumulative_kw_frequency = {}
 
     for i in range(start_video,end_video+1):
@@ -77,12 +74,15 @@ def main(start_video=1, end_video=5, file_name="keywords.txt", full_transcript="
         file_path = f"./downloads/{file}.mp4"
 
         audio_extraction(file_path)
-        speech_recognition(full_transcript)
+        speech_recognition()
         keyword_extraction(cumulative_kw_frequency)
 
     with open(file_name,"a") as file:
+        #Only record keywords that appear in multiple videos.
         for kw in cumulative_kw_frequency.keys():
-            file.write(f"{kw} {cumulative_kw_frequency[kw]}\n")
+            if cumulative_kw_frequency[kw] > 1:
+                file.write(f"{kw} {cumulative_kw_frequency[kw]}\n")
 
 if __name__ == "__main__":
-    main()
+    main(1,20)
+    main(21,40,"keywords2.txt")
