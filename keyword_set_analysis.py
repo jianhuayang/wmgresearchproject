04cb7e1d85491dee110254ebc07ccbb6e37e3e65
gongersio <samakshagarwal27@gmail.com>
2023-09-14 18:48:00 +0100

    Update comments

--- keyword_set_analysis.py
+++ keyword_set_analysis.py
@@ -1,13 +1,13 @@
-def main(file1_name,file2_name):
-    keywords_set = set() #The more popular set.
-    keywords2_set = set() #The less popular set.
-
+def main(file1_name, file2_name):
+    '''Compares two sets of keywords from text files, identifying the set difference between them. The result is then written to a text file.'''
+    keywords1_set = set() #The keywords from the more popular set.
     with open(file1_name, "r") as file:
         for line in file:
             words = line.strip().split(" ")
             keyword = " ".join(words[:-1])
-            keywords_set.add(keyword)
+            keywords1_set.add(keyword)
 
+    keywords2_set = set() #The keywords from less popular set.
     with open(file2_name, "r") as file:
         for line in file:
             words = line.strip().split(" ")
@@ -15,10 +15,10 @@ def main(file1_name,file2_name):
             keywords2_set.add(keyword)
 
     #Identify common keywords that only appear in the more popular set.
-    difference = keywords_set.difference(keywords2_set)
+    difference = keywords1_set.difference(keywords2_set)
     with open("difference.txt", "w") as file:
         for kw in difference:
             file.write(kw + "\n")
 
 if __name__ == "__main__":
-    main("keywords.txt", "keywords2.txt")
+    main("keywords1.txt", "keywords2.txt")
