c64963fe59defe3f1f7ca65766e51d4825c8592f
gongersio <samakshagarwal27@gmail.com>
2023-09-14 19:11:53 +0100

    Update comments

--- video_scene_analysis.py
+++ video_scene_analysis.py
@@ -1,7 +1,11 @@
 from scenedetect import open_video, SceneManager, AdaptiveDetector
 import pandas as pd
 
-def main(start_video=1, end_video=10):
+def main(start_video=1, end_video=40):
+    '''
+    Analyses the subset of videos from the specified file path in order to determine the number of scenes and average scene length of each video.
+    The results are then written to the video_data.csv file.
+    '''
     avg_scene_length_list = []
     num_scenes_list = []
 
@@ -14,16 +18,19 @@ def main(start_video=1, end_video=10):
         sm = SceneManager()
         sm.add_detector((AdaptiveDetector()))
         sm.detect_scenes(video)
+        scenes = sm.get_scene_list(start_in_scene=True) #Ensure that a video with no scene changes has at least one scene.
 
-        scenes = sm.get_scene_list(start_in_scene=True) 
         num_scenes = len(scenes)
         num_scenes_list.append(num_scenes)
         total_length = 0
 
+        #Calculate the total length of the video by summing up the length of each scene.
         for scene in scenes: 
-            scene_length = scene[1].get_seconds() - scene[0].get_seconds()
+            #Calculate the scene length by subtracting the start time from the end time.
+            scene_length = scene[1].get_seconds() - scene[0].get_seconds() 
             total_length+=scene_length
 
+        #Calculate the average scene length of the video.
         avg_scene_length = round(total_length/num_scenes, 2)
         avg_scene_length_list.append(avg_scene_length)
     
@@ -33,4 +40,4 @@ def main(start_video=1, end_video=10):
     df.to_csv("video_data.csv", index=False)
 
 if __name__ == "__main__":
-    main(1,40)
+    main()
